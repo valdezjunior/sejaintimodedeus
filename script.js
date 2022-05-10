96fd486be0dfb75294981f32f7824f65ebe266d0
valdezjunior <99437823+valdezjunior@users.noreply.github.com>
2022-05-10 09:51:18 -0300

    :package: estrutura

--- script.js
+++ script.js
@@ -3,3 +3,78 @@ let hamburguer = document.querySelector(".hamburguer");
 hamburguer.addEventListener("click", function () {
     document.querySelector(".container").classList.toggle("show-menu");
 });
+
+//////////////////////////////////////////////////////////////
+
+
+function dados() {
+
+    let email = document.getElementById('email');
+    let texto = document.getElementById('mensagem');
+
+
+    if (email.value.length == 0 || texto.value.length == 0) {
+        alert('Preencha todos os campos!')
+    } else {
+        alert('Dados enviados!')
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function validarEmail() {
+    let eMail = document.querySelector('#email');
+    let error = document.querySelector('#error-email');
+
+    if (!eMail.checkValidity()) {
+        error.innerHTML = "Email inválido!";
+    }
+}
+
+function redefinirMsg() {
+    let error = document.querySelector('#error-email');
+
+    if (error.innerHTML == "Email inválido!") {
+        error.innerHTML = "";
+    }
+}
+
+function confirmeEmail() {
+    let eMail = document.querySelector('#email');
+    let confEmail = document.querySelector('#emaill');
+    let errorc = document.querySelector('#error-emaill');
+
+    if (confEmail.checkValidity() !== eMail.checkValidity()) {
+        errorc.innerHTML = "Email diferente!"
+    }
+
+}
+
+function redefinirMsgm() {
+    let errorc = document.querySelector('#error-emaill');
+
+    if (errorc.innerHTML == "Email diferente!") {
+        errorc.innerHTML = "";
+    }
+}*/
+
+
+
+
